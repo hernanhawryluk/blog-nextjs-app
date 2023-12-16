76606c4934da85cdf4d5d2bbb5b44f10cbe5278d
Hernan Hawryluk <hernanhawryluk@gmail.com>
2023-12-16 16:22:04 -0300

    Little fixes before deploy

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -44,6 +44,8 @@ export const authOptions: AuthOptions = {
 
 const handler = NextAuth(authOptions);
 
+export default NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
 
 export const getAuthSession = () => {
