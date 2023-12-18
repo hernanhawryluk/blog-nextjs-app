52972a15c2db52e5b4a721e8b455682326e05b40
Hernan Hawryluk <hernanhawryluk@gmail.com>
2023-12-18 17:40:10 -0300

    Small fixes for better SEO

--- app/sitemaps.ts
+++ app/sitemaps.ts
@@ -1,30 +1,12 @@
 import { MetadataRoute } from "next";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-const getData = async (page: number, cat: string) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts?page=${page}&cat=${cat}`
-  );
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-};
-
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const { posts, count } = await getData(1, "");
-
-  const urls: string[] = [BASE_URL as string];
-
-  for (const post of posts) {
-    urls.push(`${BASE_URL}/posts/${post.slug}`);
-  }
-
-  return urls.map((url) => ({
-    url,
-    lastModified: new Date(),
-  }));
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: process.env.NEXT_PUBLIC_BASE_URL as string,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+  ];
 }
